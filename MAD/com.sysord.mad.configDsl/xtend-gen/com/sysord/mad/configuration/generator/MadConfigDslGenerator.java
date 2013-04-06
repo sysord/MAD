9b0a4e1c103a78ec06007bb69dc00f0d49c51c78
Fabien <vignalf@gmail.com>
2013-04-06 17:59:47 +0200

    Add xtend-gen folder.

---
+++ MAD/com.sysord.mad.configDsl/xtend-gen/com/sysord/mad/configuration/generator/MadConfigDslGenerator.java
@@ -0,0 +1,22 @@
+/***************************************************************************
+ * Copyright (c) 2013 Sysord.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Sysord - initial API and implementation
+ ****************************************************************************/
+package com.sysord.mad.configuration.generator;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IGenerator;
+
+@SuppressWarnings("all")
+public class MadConfigDslGenerator implements IGenerator {
+  public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
+  }
+}
