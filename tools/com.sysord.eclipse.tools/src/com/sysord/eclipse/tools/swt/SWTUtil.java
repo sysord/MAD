cf21a651486517a67010095a408db1bf2ab20fc8
Fabien <vignalf@gmail.com>
2013-06-18 11:10:19 +0200

    Modification of the copyright header.

--- tools/com.sysord.eclipse.tools/src/com/sysord/eclipse/tools/swt/SWTUtil.java
+++ tools/com.sysord.eclipse.tools/src/com/sysord/eclipse/tools/swt/SWTUtil.java
@@ -1,8 +1,14 @@
-/*
- * SWTUtil.java                                        17 déc. 2012
+/***************************************************************************
+ * Copyright (c) 2013 Sysord.
  * 
- * Copyright (c) 2012 Sysord
- */
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Sysord - initial API and implementation
+ ****************************************************************************/
 package com.sysord.eclipse.tools.swt;
 
 import java.io.IOException;
